4b1c8812a459c8bc2f56ac40e7c9f29253bc45bc
Aaron <iaaron.xyz@outlook.com>
2022-07-25 12:38:28 -0500

    pset0 indoor.py finished

---
+++ pset0/indoor/indoor.py
@@ -0,0 +1,15 @@
+"""
+Usage: $ python3 indoor.py
+
+Description:
+Receives an input string and
+returns it in lowercase
+
+Examples:
+$ python3 indoor.py
+HEeLloO!
+heelloo!
+"""
+
+str1 = input()
+print(f"{str1.lower()}")
